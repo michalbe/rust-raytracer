311cc48fbd6a83c65b3e9d68282df8ed0e79a0d6
Michał Budzyński <michal@virtualdesign.pl>
2019-04-08 20:35:00 +0100

    :raising_hand: render example ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let max_i = 200;
+    let max_j = 200;
+
+    println!("P3\n{} {} 255", max_i, max_j);
+    for i in 0..max_i {
+        for j in 0..max_j {
+            let r: f32 = i as f32 / max_i as f32;
+            let g: f32 = j as f32 / max_j as f32;
+            let b: f32 = 0.2;
+
+            let ir = (255.00 * r).round();
+            let ig = (255.00 * g).round();
+            let ib = (255.00 * b).round();
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
