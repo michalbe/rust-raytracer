32f6cfae407f8b7746e274c0aa6e81b8e17aaa7d
Michał Budzyński <michal@virtualdesign.pl>
2019-04-08 20:46:00 +0100

    :octocat: use vec3D

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate vec3D;
+
+use vec3D::Vec3D;
+
 fn main() {
     let max_i = 200;
     let max_j = 200;
@@ -5,14 +9,12 @@ fn main() {
     println!("P3\n{} {} 255", max_i, max_j);
     for i in 0..max_i {
         for j in 0..max_j {
-            let r: f32 = i as f32 / max_i as f32;
-            let g: f32 = j as f32 / max_j as f32;
-            let b: f32 = 0.2;
 
-            let ir = (255.00 * r).round();
-            let ig = (255.00 * g).round();
-            let ib = (255.00 * b).round();
-            println!("{} {} {}", ir, ig, ib);
+            let col = Vec3D::new(i as f64 / max_i as f64, j as f64 / max_j as f64, 0.2);
+            let r = (255.00 * col.x).round();
+            let g = (255.00 * col.y).round();
+            let b = (255.00 * col.z).round();
+            println!("{} {} {}", r, g, b);
         }
     }
 }
