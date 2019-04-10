75d78c65c8c5a8ad9c509ea925e226d845464682
Michał Budzyński <michal@virtualdesign.pl>
2019-04-10 14:49:00 +0100

    :dart: sphere class

---
+++ src/sphere.rs
@@ -0,0 +1,39 @@
+use crate::{
+    hitable::{HitRecord, Hitable},
+    ray::Ray,
+};
+
+use vec3D::Vec3D;
+
+pub struct Sphere {
+    pub center: Vec3D,
+    pub radius: f64,
+}
+
+impl Hitable for Sphere {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let oc = ray.origin - self.center;
+        let a = ray.direction.dot(ray.direction);
+        let b = oc.dot(ray.direction);
+        let c = oc.dot(oc) - self.radius * self.radius;
+        let discriminant = b * b - a * c;
+        if discriminant > 0.0 {
+            let mut temp = (-b - (b * b - a * c).sqrt()) / a;
+            if temp < t_max && temp > t_min {
+                rec.t = temp;
+                rec.p = ray.point_at_parameter(rec.t);
+                rec.normal = (rec.p - self.center) / self.radius;
+                return true;
+            }
+            temp = (-b + (b * b - a * c).sqrt()) / a;
+            if temp < t_max && temp > t_min {
+                rec.t = temp;
+                rec.p = ray.point_at_parameter(rec.t);
+                rec.normal = (rec.p - self.center) / self.radius;
+                return true;
+            }
+        }
+
+        false
+    }
+}
