50fa246749166e93efc746a70fcf0bc1eb99a3d4
Michał Budzyński <michal@virtualdesign.pl>
2019-04-11 14:50:00 +0100

    :wave: hitable trait

---
+++ src/hitable.rs
@@ -0,0 +1,46 @@
+use crate::ray::Ray;
+use vec3D::Vec3D;
+
+pub struct HitRecord {
+    pub t: f64,
+    pub p: Vec3D,
+    pub normal: Vec3D,
+}
+
+impl HitRecord {
+    pub fn new() -> HitRecord {
+        HitRecord {
+            t: 0.0,
+            p: Vec3D::new(0.0, 0.0, 0.0),
+            normal: Vec3D::new(0.0, 0.0, 0.0),
+        }
+    }
+}
+
+pub trait Hitable {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
+}
+
+pub struct HitableList {
+    pub list: Vec<Box<Hitable>>
+}
+
+impl Hitable for HitableList {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::new();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for element in self.list.iter() {
+            if element.hit(ray, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec.p = temp_rec.p;
+                rec.t = temp_rec.t;
+                rec.normal = temp_rec.normal;
+            }
+        }
+
+        hit_anything
+    }
+}
