9fce4a6f73cd45e21b62cd10e0a5c19dd2890a55
Michał Budzyński <michal@virtualdesign.pl>
2019-04-11 14:51:00 +0100

    :traffic_light: render everything using new logic

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,29 @@
 extern crate vec3D;
 
 mod ray;
+mod hitable;
+mod sphere;
 
 use vec3D::Vec3D;
 use ray::Ray;
+use hitable::{HitRecord, Hitable, HitableList};
+use sphere::Sphere;
 
-fn hit_sphere(center: Vec3D, radius: f64, ray: &Ray) -> f64 {
-    let oc = ray.origin - center;
-    let a = ray.direction.dot(ray.direction);
-    let b = oc.dot(ray.direction) * 2.0;
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    if discriminant < 0.0 {
-        return -1.0;
+fn color(ray: Ray, world: &Hitable) -> Vec3D {
+    let mut rec = HitRecord::new();
+    if world.hit(&ray, 0.0, std::f64::MAX, &mut rec) {
+        return Vec3D::new(rec.normal.x + 1.0, rec.normal.y + 1.0, rec.normal.z + 1.0) * 0.5;
     } else {
-        return (-b - discriminant.sqrt()) / (2.0 * a);
+        let unit_direction = ray.direction.unit();
+        let t = 0.5 * (unit_direction.y + 1.0);
+        return (Vec3D::new(1.0, 1.0, 1.0) * (1.0 - t)) + (Vec3D::new(0.5, 0.7, 1.0) * t);
     }
-}
-
-fn color(ray: Ray) -> Vec3D {
-    let t = hit_sphere(Vec3D::new(0.0, 0.0, -1.0), 0.5, &ray);
-    if t > 0.0 {
-        let normal = (ray.point_at_parameter(t) - Vec3D::new(0.0, 0.0, -1.0)).unit();
-        return Vec3D::new(normal.x + 1.0, normal.y + 1.0, normal.z + 1.0) * 0.5;
-    }
-
-    let unit_direction = ray.direction.unit();
-    let t = 0.5 * (unit_direction.y + 1.0);
-    (Vec3D::new(1.0, 1.0, 1.0) * (1.0 - t)) + (Vec3D::new(0.5, 0.7, 1.0) * t)
 }
 
 
 fn main() {
-    let nx = 200;
-    let ny = 100;
+    let nx = 400;
+    let ny = 200;
 
     println!("P3\n{} {} 255", nx, ny);
 
@@ -42,12 +32,27 @@ fn main() {
     let vertical = Vec3D::new(0.0, 2.0, 0.0);
     let origin = Vec3D::new(0.0, 0.0, 0.0);
 
+    let mut world = HitableList {
+        list: vec![]
+    };
+
+    world.list.push(Box::new(Sphere {
+        center: Vec3D::new(0.0, 0.0, -1.0),
+        radius: 0.5,
+    }));
+
+    world.list.push(Box::new(Sphere {
+        center: Vec3D::new(0.0, -100.5, -1.0),
+        radius: 100.0,
+    }));
+
     for j in (0..ny).rev() {
         for i in 0..nx {
             let u = i as f64 / nx as f64;
             let v = j as f64 / ny as f64;
             let r = Ray::new(origin, lower_left_corner + (horizontal * u) + (vertical * v));
-            let col = color(r);
+
+            let col = color(r, &world);
             // println!("{}, t", col);
             let r = (255.00 * col.x).round();
             let g = (255.00 * col.y).round();
