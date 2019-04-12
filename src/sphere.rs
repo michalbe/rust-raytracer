43f65985fc9a4efa282594b40a9e906ae33c7e2b
Michał Budzyński <michal@virtualdesign.pl>
2019-04-12 20:04:00 +0100

    :construction: pass materials in spheres

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,6 +1,7 @@
 use crate::{
     hitable::{HitRecord, Hitable},
     ray::Ray,
+    material::Material,
 };
 
 use vec3D::Vec3D;
@@ -8,6 +9,21 @@ use vec3D::Vec3D;
 pub struct Sphere {
     pub center: Vec3D,
     pub radius: f64,
+    material: Material,
+}
+
+impl Sphere {
+    pub fn new(center: Vec3D, radius: f64, material: Material) -> Sphere {
+        Sphere {
+            center,
+            radius,
+            material,
+        }
+    }
+
+    // pub fn material(&self) -> Material {
+    //     self.material
+    // }
 }
 
 impl Hitable for Sphere {
@@ -23,6 +39,7 @@ impl Hitable for Sphere {
                 rec.t = temp;
                 rec.p = ray.point_at_parameter(rec.t);
                 rec.normal = (rec.p - self.center) / self.radius;
+                rec.material = self.material;
                 return true;
             }
             temp = (-b + (b * b - a * c).sqrt()) / a;
