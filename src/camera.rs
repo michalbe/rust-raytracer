41f4982eb541aed4841812eb7b20d4a71b9f5e69
Michał Budzyński <michal@virtualdesign.pl>
2019-04-13 13:59:00 +0100

    :blue_car: camera position & look_at

--- src/camera.rs
+++ src/camera.rs
@@ -9,20 +9,25 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(vfov: f64, aspect: f64) -> Camera {
+    pub fn new(look_from: Vec3D, look_at: Vec3D, up: Vec3D, vfov: f64, aspect: f64) -> Camera {
         let theta = vfov * std::f64::consts::PI / 180.0;
         let half_height = (theta / 2.0).tan();
         let half_width = aspect * half_height;
 
+        let w = (look_from - look_at).unit();
+        let u = up.cross(w).unit();
+        let v = w.cross(u);
+        let origin = look_from;
+
         Camera {
-            lower_left_corner: Vec3D::new(-half_width, -half_height, -1.0),
-            horizontal :Vec3D::new(2.0 * half_width, 0.0, 0.0),
-            vertical: Vec3D::new(0.0, 2.0 * half_height, 0.0),
-            origin: Vec3D::new(0.0, 0.0, 0.0),
+            lower_left_corner: origin - u * half_width - v * half_height - w,
+            horizontal: u * half_width * 2.0,
+            vertical: v * half_height * 2.0,
+            origin,
         }
     }
 
     pub fn get_ray(&self, u: f64, v: f64) -> Ray {
-        Ray::new(self.origin, self.lower_left_corner + (self.horizontal * u) + (self.vertical * v))
+        Ray::new(self.origin, self.lower_left_corner + (self.horizontal * u) + (self.vertical * v) - self.origin)
     }
 }
