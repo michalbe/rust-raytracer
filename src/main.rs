ba045fcba766a321b7e9a6fbc57077406f1f4d50
Michał Budzyński <michal@virtualdesign.pl>
2019-04-13 18:43:00 +0100

    :evergreen_tree: add par_iters

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 extern crate vec3D;
 extern crate rand;
+extern crate rayon;
+
+use vec3D::Vec3D;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 mod utils;
 mod ray;
@@ -8,7 +12,6 @@ mod sphere;
 mod camera;
 mod material;
 
-use vec3D::Vec3D;
 use hitable::HitableList;
 use sphere::Sphere;
 use camera::Camera;
@@ -24,7 +27,7 @@ use utils::color;
 fn main() {
     let nx = 600;
     let ny = 300;
-    let ns = 200;
+    let ns = 100;
 
     println!("P3\n{} {} 255", nx, ny);
 
@@ -33,11 +36,11 @@ fn main() {
         list: vec![]
     };
 
-    world.list.push(Box::new(Sphere::new(
+    world.list.push(Sphere::new(
         Vec3D::new(0.0, -1000.0, 0.0),
         1000.0,
         Material::Lambert(Lambert::new(Vec3D::new(0.5, 0.5, 0.5)))
-    )));
+    ));
 
     for a in -11..11 {
         for b in -11..11 {
@@ -45,7 +48,7 @@ fn main() {
             let center = Vec3D::new((a as f64) + 0.9 * rand::random::<f64>(), 0.2, (b as f64) + 0.9 * rand::random::<f64>());
             if (center - Vec3D::new(4.0, 0.2, 0.0)).mag() > 0.9 {
                 if material < 0.8 {
-                    world.list.push(Box::new(Sphere::new(
+                    world.list.push(Sphere::new(
                         center,
                         0.2,
                         Material::Metal(Metal::new(Vec3D::new(
@@ -53,9 +56,9 @@ fn main() {
                             0.5 * (1.0 + rand::random::<f64>()),
                             0.5 * (1.0 + rand::random::<f64>())
                         ), 0.5 * rand::random::<f64>()))
-                    )));
+                    ));
                 } else {
-                    world.list.push(Box::new(Sphere::new(
+                    world.list.push(Sphere::new(
                         center,
                         0.2,
                         Material::Lambert(Lambert::new(Vec3D::new(
@@ -63,28 +66,28 @@ fn main() {
                             rand::random::<f64>() * rand::random::<f64>(),
                             rand::random::<f64>() * rand::random::<f64>()
                         )))
-                    )));
+                    ));
                 }
             }
         }
     }
-    world.list.push(Box::new(Sphere::new(
+    world.list.push(Sphere::new(
         Vec3D::new(0.0, 1.0, 0.0),
         1.0,
         Material::Dielectric(Dielectric::new(1.5))
-    )));
+    ));
 
-    world.list.push(Box::new(Sphere::new(
+    world.list.push(Sphere::new(
         Vec3D::new(-4.0, 1.0, 0.0),
         1.0,
         Material::Lambert(Lambert::new(Vec3D::new(0.4, 0.2, 0.1)))
-    )));
+    ));
 
-    world.list.push(Box::new(Sphere::new(
+    world.list.push(Sphere::new(
         Vec3D::new(4.0, 1.0, 0.0),
         1.0,
         Material::Metal(Metal::new(Vec3D::new(0.7, 0.6, 0.5), 0.0))
-    )));
+    ));
 
 
     let look_from = Vec3D::new(13.0, 2.0, 3.0);
@@ -102,8 +105,9 @@ fn main() {
         distance_to_focus
     );
 
-    for j in (0..ny).rev() {
-        for i in 0..nx {
+    let pixels: Vec<Vec<(f64, f64, f64)>> =  (0..ny).into_par_iter().map(|j| {
+        let j = ny as f64 - j as f64 - 1.0;
+        let row: Vec<(f64, f64, f64)> = (0..nx).into_par_iter().map(|i| {
             let mut col = Vec3D::new(0.0, 0.0, 0.0);
 
             for _ in 0..ns {
@@ -118,6 +122,16 @@ fn main() {
             let r = (255.00 * col.x.sqrt()).round();
             let g = (255.00 * col.y.sqrt()).round();
             let b = (255.00 * col.z.sqrt()).round();
+
+            (r, g, b)
+        }).collect();
+
+        row
+
+    }).collect();
+
+    for row in pixels {
+        for (r, g, b) in row {
             println!("{} {} {}", r, g, b);
         }
     }
